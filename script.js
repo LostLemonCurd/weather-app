7b196e18e62d9e4dc7affc05b4efdd194b5781a4
Lounis Ourrad <109440184+LostLemonCurd@users.noreply.github.com>
2023-02-25 17:46:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ search.addEventListener('click', () => {
             weatherBox.style.display = 'none';
             error404.style.display = 'block';
             error404.classList.add('fadeIn');
+            return;
         }
 
         error404.style.display = 'none';
